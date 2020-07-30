7d475ce059f982fae64d68004c986369825488f2
Shuquan Huang <shuquanh9009@hstat.org>
2020-07-30 18:17:56 +0000

    update results and ticker html

--- app.py
+++ app.py
@@ -3,11 +3,15 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 from datetime import datetime
 import finnhub
 import requests
 import os
+from flask import session
 from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+import bcrypt
 
 # finnhub_client = finnhub.Client(api_key="bscrtk7rh5rcu5phgmc0")
 
@@ -16,12 +20,51 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv()
 finnhub_client = os.getenv("finnhub_key")
+app.secret_key = os.getenv("SECRET_KEY")
+Mongo_User = os.getenv("Mongo_User")
+Mongo_Password = os.getenv("Mongo_Password")
+app.config['MONGO_DBNAME'] = 'login'
+
+
+app.config['MONGO_URI'] = f'mongodb+srv://{Mongo_User}:{Mongo_Password}@cluster0.a3ibi.mongodb.net/login?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html",time=datetime.now())
+@app.route('/home')
+def home():
+    return render_template("home.html", time=datetime.now())
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    users = mongo.db.users
+    hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+   
+    # existing_user = users.find_one({'name' : request.form['name']})
+
+    if users.find_one({'name' : request.form['name']}):
+        return "This name is taken, try another name!"
+    else: 
+        users.insert({"name": request.form["name"], "password": str(hashpass, 'utf-8')})
+        session["user"] = request.form["name"]
+        
+        return "success"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        users = mongo.db.users
+        login_user = users.find_one({'name' : request.form['name']})
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+                session['user'] = request.form['name']
+                return "success"
+        
+        # if no successful match, display an error 
+        return redriect('/index')
+
     
 @app.route('/survey')
 def survey():
@@ -76,16 +119,9 @@ def result():
         print(company)
         print(field)
 
-        # name = requests.get(f'https://finnhub.io/api/v1/stock/profile2?symbol=UNH&token={finnhub_client}').json()
-       
 
         companies = requests.get(f'https://finnhub.io/api/v1/stock/peers?symbol={company}&token={finnhub_client}').json()
         # goes through every company in the companies list
-        # for i in range(0,len(companies)-2):
-        #     # adds each company's price dictionary into a list called prices
-        #     # adds each company's low,high, and annual return into stats list
-        #     c=companies[i]
-        #     print(c)
         for c in companies:
             # adds each company's price dictionary into a list called prices
             # adds each company's low,high, and annual return into stats list
@@ -104,16 +140,6 @@ def result():
                     aPrices.append(prices[i])
                     affordStats.append(stats[i])
 
-        # print(affordable)
-        # for a in affordable:
-        #     aPrices.append(requests.get(f'https://finnhub.io/api/v1/quote?symbol={a}&token={finnhub_client}').json())
-        #     affordStats.append(requests.get(f'https://finnhub.io/api/v1/stock/metric?symbol={a}&metric=all&token={finnhub_client}').json())
-        #     affordNames.append(requests.get(f'https://finnhub.io/api/v1/stock/profile2?symbol={a}&token={finnhub_client}').json())
-
-        # price = data['c']
-        # industry = data1['finnhubIndustry']
-        # print(data1)
-        # print(data)
         print(companies)
         print(affordable)
         print(aPrices)
